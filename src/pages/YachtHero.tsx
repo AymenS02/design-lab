93e5b1d6f39aca98ccef57a1ac9ff9bdd1a5653c
Aymen Shoteri <95427313+AymenS02@users.noreply.github.com>
2026-09-24 13:48:51 -0400

    fixed vercel issues

--- src/pages/YachtHero.tsx
+++ src/pages/YachtHero.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { AnchorIcon } from "@hugeicons/core-free-icons";
 import gsap from "gsap";
